6d3a61921491fb6489131d66dd5b53830d430071
Mack <macklin@beasleys.net.au>
2025-08-27 19:09:34 +1000

    moved probe down 4mm

--- scripts/generators/tank_probe.py
+++ scripts/generators/tank_probe.py
@@ -3,10 +3,10 @@ import numpy as np
 
 TRAVEL_SPEED=5000
 PROBE_SPEED=250
-START_Z = -76.5     # xy probing depth = -72.5 z swtich to spoilboard + 12 - 16 mm probing point min tank thickness
+START_Z = -100     # xy probing depth = -72.5 z swtich to spoilboard + 10 - 16 mm probing point min tank thickness - z probe from xy (21mm)
 OFFSET_XY_MM = 10   # distance to retract from tank after probing xy
-OFFSET_Z_MM = 52    # 21mm from z to xy switch + 15mm clearnce + 16mm offset measuremtn pt
-OFFSET_XY_SWITCH_MM = 27.4 # distance from xy switch to z probe position = 11.4 (switch z delta) + 16 (grove inset dist)
+OFFSET_Z_MM = 74.5    # 21mm from z to xy switch + 15mm clearnce + 38.5 offset measuremtn pt
+OFFSET_XY_SWITCH_MM = 37.4 # distance from xy switch to z probe position = 11.4 (switch z delta) + 16 (grove inset dist) + OFFSET_X_MM (10)
 
 def shell_probe(type):
     inner_coords = np.loadtxt(f"{type}concentric_inner.csv", delimiter=',', skiprows=1)     # Load inner coordinates (X,Y)
